12e2ad7ca6b7fbf8bd3e23110a269c137094e7b3
Ross Condie <rosscondie@gmail.com>
2023-02-08 15:46:18 +0100

    added data order_list

---
+++ models/order_list.py
@@ -0,0 +1,9 @@
+from models.order import Order
+
+order_list = [
+    Order("James Hetfield", "25.12.1980", "Microphone", 1),
+    Order("Kirk Hammett", "20.12.1980", "Guitar", 3),
+    Order("Lars Ulrich", "13.12.1975", "DrumKit", 2),
+    Order("The Crab", "12.11.1986", "BassGuitar", 3)
+
+]
