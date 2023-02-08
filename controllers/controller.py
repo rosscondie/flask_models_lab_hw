5373621e05c49e8dafe70fe6acaa7628a19115bf
Ross Condie <rosscondie@gmail.com>
2023-02-08 16:04:37 +0100

    added index.html

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,7 @@
 from app import app
+from flask import render_template
+from models.order_list import order_list
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template("index.html", order_list = order_list)
