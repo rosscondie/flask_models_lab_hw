3ad9b33fda5361f636b5ca1b00037a1150df6efb
Ross Condie <rosscondie@gmail.com>
2023-02-08 15:36:47 +0100

    added order class

---
+++ models/order.py
@@ -0,0 +1,6 @@
+class Order:
+    def __init__(self, customer_name, order_date, item_purchased, quantity):
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.item_purchased = item_purchased
+        self.quantity = quantity
